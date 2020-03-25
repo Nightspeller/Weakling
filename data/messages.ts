463304299864551815e12922bac6037f7bf125a8
Serg <serj.nights@gmail.com>
2020-03-24 23:15:07 -0400

    Apple garden added

--- data/messages.ts
+++ data/messages.ts
@@ -3,6 +3,7 @@ export const messages = {
     caltor_waymark: `West - to the fort.\nEast - to the fields and tavern.\nNorth - to Honeywood.`,
 
     betweenVillageAndCaltor_waymark: `West - to Caltor.\nEast - to Mundriv forest.\nNorth - to Alahai Mountains (don't step on Kobolds!).\nSouth - to Great Planes.`,
+    betweenVillageAndCaltor_garden_sign: `Property of farmer Joe. \n\nUnauthorized pickers will be hit with the broom!`,
 
     village_boarWarning: `Hmm.. These boars look peaceful from here, but I am sure that if I try to hunt them they will chase me up to the tree and will not let me down for at least several hours..like last time...`,
     village_placeOfMemory: `May thous who came before us continue guiding us.\n\n\n(It is always quiet and peaceful here, for this is a place where ancestors of your village are resting in peace.\nAuntie Nahkha keeps an eye on it clearing the leaves and planting blue Primulas as a sign of memory and gratitute to those who came before us.)`
