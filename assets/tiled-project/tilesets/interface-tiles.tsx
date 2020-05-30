04f92bf2605adc19fc4ad9606a1db59639799625
Serg <serj.nights@gmail.com>
2020-05-29 20:44:21 -0400

    File adjustments after tiled project restructuring

--- assets/tiled-project/tilesets/interface-tiles.tsx
+++ assets/tiled-project/tilesets/interface-tiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="interface-tiles" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="18" columns="6">
- <image source="../images-extruded/interface/interface-tiles.png" width="204" height="102"/>
+<tileset version="1.2" tiledversion="2020.05.27" name="interface-tiles" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="18" columns="6">
+ <image source="../../images-extruded/interface/interface-tiles.png" width="204" height="102"/>
  <terraintypes>
   <terrain name="New Terrain" tile="7"/>
  </terraintypes>
--- scenes/preload.ts
+++ scenes/preload.ts
@@ -226,7 +226,7 @@ export class PreloadScene extends Phaser.Scene {
         console.log('Preload done, calling Main Menu');
         //this.scene.start("Battle", {enemies: [{"type": "wildBoar"}, {"type": "wizard"}, {"type": "wizard"}, {"type": "wildBoar"}], prevScene: "Caltor"});
         if (DEBUG) {
-            this.scene.start("Village", {prevScene: this.scene.key});
+            this.scene.start("WeaklingsCave", {prevScene: this.scene.key});
         } else {
             this.scene.start("MainMenu", {prevScene: this.scene.key});
         }
