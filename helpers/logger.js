5a493c3ae053ec1132679d11af548d0e50a9edfc
Serg <serj.nights@gmail.com>
2020-06-09 15:39:54 -0400

    Better logger colors

--- helpers/logger.js
+++ helpers/logger.js
@@ -4,13 +4,13 @@ export default function prepareLog(text, timestamp = false) {
     const colors = [];
     let replacedText = text.replace(/(!!|\?\?([^]))\w+/g, (value) => {
         value[0] === '!' ? colors.push('color: red') : colors.push('color: aqua');
-        colors.push('color: white');
+        colors.push('color: auto');
         return `%c${value.substr(2)}%c`;
     });
     if (timestamp) {
         const timestampString = `%c[${(new Date).getHours()}:${(new Date).getMinutes()}:${(new Date).getSeconds()}]%c `;
         replacedText = timestampString + replacedText;
-        colors.unshift('color: green', 'color: white');
+        colors.unshift('color: green', 'color: auto');
     }
     return [replacedText, ...colors];
 }
