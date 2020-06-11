ed1c0c1b0699bdc425726bc0c70b8a893beae5a3
Serg <serj.nights@gmail.com>
2020-06-11 15:53:22 -0400

    Logger updated to properly highlight words with - and '

--- helpers/logger.ts
+++ helpers/logger.ts
@@ -1,7 +1,7 @@
 export default function prepareLog(text: any, timestamp = false) {
     if (typeof text !== "string") return [text];
     const colors = [];
-    let replacedText = text.replace(/(!!|\?\?([^]))\w+/g, (value) => {
+    let replacedText = text.replace(/(!!|\?\?([^]))(\w|'|-)+/g, (value) => {
         value[0] === '!' ? colors.push('color: red') : colors.push('color: aqua');
         colors.push('color: auto');
         return `%c${value.substr(2)}%c`
