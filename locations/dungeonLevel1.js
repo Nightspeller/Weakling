16b314a4f659ea780cec49ecdcb3ab291814a322
Serg <serj.nights@gmail.com>
2020-06-30 17:28:57 -0400

    Some missing filed added for audio

--- locations/dungeonLevel1.js
+++ locations/dungeonLevel1.js
@@ -5,12 +5,16 @@ export class DungeonLevel1Scene extends GeneralLocation {
     }
     preload() {
         super.preload();
+        this.load.audio('labyrinth-of-lost-dreams', ['assets/audio/labyrinth-of-lost-dreams.mp3', 'assets/audio/keys-for-success.ogg']);
     }
     init(data) {
         super.init(data);
     }
     create() {
         super.create('dungeonLevel1');
+        const bgMusic = this.sound.add('labyrinth-of-lost-dreams', { loop: true, volume: 0.1 });
+        bgMusic['soundType'] = 'music';
+        bgMusic.play();
     }
     update() {
         super.update();
