3480ee6442d212b2efbdecbe33d3a2780fd7cfe1
Johannes Magnusson <johannes-magnusson@hotmail.com>
2021-05-22 19:33:00 +0200

    Added a new title screen to the game

--- src/scenes/preload/preloadAudioAssets.ts
+++ src/scenes/preload/preloadAudioAssets.ts
@@ -2,6 +2,12 @@ import { Scene } from 'phaser';
 
 export default function preloadAudioAssets(preloadScene: Scene) {
   // Audio
+  preloadScene.load.audio('main-menu-theme', ['assets/audio/main-menu.ogg', 'assets/audio/main-menu.mp3']);
+  preloadScene.load.audio('text-button-hover', 'assets/audio/ui/text-button-hover.ogg');
+  preloadScene.load.audio('text-button-select', 'assets/audio/ui/text-button-select.ogg');
+  preloadScene.load.audio('checkbox-checked', 'assets/audio/ui/checkbox-checked.ogg');
+  preloadScene.load.audio('main-menu-start-game', 'assets/audio/ui/main-menu-start-game.ogg');
+
   preloadScene.load.audio('intro', ['assets/audio/intro.ogg', 'assets/audio/intro.mp3']);
   preloadScene.load.audio('keys-for-success', ['assets/audio/keys-for-success.mp3', 'assets/audio/keys-for-success.ogg']);
 
--- src/scenes/preload/preloadInterfaceAndIconsAssets.ts
+++ src/scenes/preload/preloadInterfaceAndIconsAssets.ts
@@ -1,18 +1,27 @@
 import * as Phaser from 'phaser';
 import { Scene } from 'phaser';
-import { tilesetConfig } from '../../config/constants';
+import { tilesetConfig, tilesetConfig64x64 } from '../../config/constants';
 
 export function preloadInterfaceAndIconsAssets(preloadScene: Scene) {
   preloadScene.load.image('doll', 'assets/images/interface/doll.png');
   preloadScene.load.image('inventory-slot', 'assets/images/interface/inventory-slot.png');
 
   preloadScene.load.image('interface-24x19', 'assets/images-extruded/interface/interface-24x19.png');
-  preloadScene.load.image('main-menu-background', 'assets/images/interface/main-menu-background.jpg');
+  preloadScene.load.image('main-menu-logo', 'assets/images/interface/main-menu-logo.png');
+  preloadScene.load.image('main-menu-sky', 'assets/images/interface/main-menu-sky.png');
+  preloadScene.load.image('main-menu-mountains', 'assets/images/interface/main-menu-mountains.png');
+  preloadScene.load.image('clouds', 'assets/images/interface/main-menu-clouds.png');
+  preloadScene.load.image('checkbox-checked', 'assets/images/interface/checkbox-checked.png');
+  preloadScene.load.image('checkbox-unchecked', 'assets/images/interface/checkbox-unchecked.png');
+
   preloadScene.load.image('fishing-background', 'assets/images/interface/fishing-background.png');
 
   // Items
   preloadScene.load.spritesheet('icon-item-set', 'assets/images-extruded/interface/icon-item-set.png', tilesetConfig);
   preloadScene.load.spritesheet('icon-farming-set', 'assets/images-extruded/interface/icon-farming-set.png', tilesetConfig);
+
+  // paper scroll background
+  preloadScene.load.spritesheet('paper-scroll-background', 'assets/images-extruded/interface/paper-scroll-background.png', tilesetConfig64x64);
 
   // Icons atlas - Assets for this atlas are under top level assets folder (assets/ftp-project/icons) and compiled during build by Webpack, on the fly.
   preloadScene.load.atlas('icons',
