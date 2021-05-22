f6e537201605f46fc604976e91412adbdb415160
Johannes Magnusson <johannes-magnusson@hotmail.com>
2021-05-22 19:05:32 +0200

    Added a new font for the title screen

--- src/scenes/preload/preloadFonts.ts
+++ src/scenes/preload/preloadFonts.ts
@@ -2,4 +2,5 @@ import { Scene } from 'phaser';
 
 export default function preloadFonts(preloadScene: Scene) {
   preloadScene.load.bitmapFont('bitmapArial', 'assets/bitmap-fonts/bitmapArial16.png', 'assets/bitmap-fonts/bitmapArial16.fnt');
+  preloadScene.load.bitmapFont('harrington', 'assets/bitmap-fonts/harrington.png', 'assets/bitmap-fonts/harrington.fnt');
 }
