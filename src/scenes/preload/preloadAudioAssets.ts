2bbe452d12bf1ea67b3e4aa05471d88f66cf73e7
Johannes Magnusson <johannes-magnusson@hotmail.com>
2021-06-09 19:01:11 +0200

    Added music for the cutscene about Evelyn's dad

--- src/scenes/preload/preloadAudioAssets.ts
+++ src/scenes/preload/preloadAudioAssets.ts
@@ -1,8 +1,13 @@
 import { Scene } from 'phaser';
 
 export default function preloadAudioAssets(preloadScene: Scene) {
-  // Audio
+  // Songs
   preloadScene.load.audio('main-menu-theme', ['assets/audio/main-menu.ogg', 'assets/audio/main-menu.mp3']);
+  preloadScene.load.audio('evelyns-story', ['assets/audio/evelyns-story.ogg', 'assets/audio/evelyns-story.mp3']);
+  preloadScene.load.audio('intro', ['assets/audio/intro.ogg', 'assets/audio/intro.mp3']);
+  preloadScene.load.audio('keys-for-success', ['assets/audio/keys-for-success.ogg', 'assets/audio/keys-for-success.mp3']);
+
+  // Sound effects
   preloadScene.load.audio('text-button-hover', ['assets/audio/ui/text-button-hover.ogg', 'assets/audio/ui/text-button-hover.mp3']);
   preloadScene.load.audio('text-button-select', ['assets/audio/ui/text-button-select.ogg', 'assets/audio/ui/text-button-select.mp3']);
   preloadScene.load.audio('checkbox-checked', ['assets/audio/ui/checkbox-checked.ogg', 'assets/audio/ui/checkbox-checked.mp3']);
@@ -10,9 +15,6 @@ export default function preloadAudioAssets(preloadScene: Scene) {
   preloadScene.load.audio('typewriter-short', ['assets/audio/ui/typewriter-short.ogg', 'assets/audio/ui/typewriter-short.mp3']);
   preloadScene.load.audio('typewriter-end', ['assets/audio/ui/typewriter-end.ogg', 'assets/audio/ui/typewriter-end.mp3']);
   preloadScene.load.audio('main-menu-start-game', ['assets/audio/ui/main-menu-start-game.ogg', 'assets/audio/ui/main-menu-start-game.mp3']);
-
-  preloadScene.load.audio('intro', ['assets/audio/intro.ogg', 'assets/audio/intro.mp3']);
-  preloadScene.load.audio('keys-for-success', ['assets/audio/keys-for-success.ogg', 'assets/audio/keys-for-success.mp3']);
 
   preloadScene.load.audio('fishing-bait', ['assets/audio/fishing/fishing-bait.ogg', 'assets/audio/fishing/fishing-bait.mp3']);
   preloadScene.load.audio('fishing-success', ['assets/audio/fishing/fishing-success.ogg', 'assets/audio/fishing/fishing-success.mp3']);
