0522aa9c24b2c6078fc651dfc6293ff278991a5c
unknown <ctcrawford93@yahoo.com>
2021-06-21 18:32:18 -0400

    Proofreading for planting patch dialog and modified eyeball dialog for reusable strings

--- src/data/dialogs/backCave/eyeballDialog.ts
+++ src/data/dialogs/backCave/eyeballDialog.ts
@@ -1,82 +1,101 @@
 import { DialogTree } from '../../../types/my-types';
 
+const eyeballActor = {
+  helloFriend: 'Bul\'k bul\'k!',
+  theAudacity: 'Buuuul\'k...',
+  howCouldYou: 'Bul\'k! Bul\'k! Bul\'k!',
+  unbelievable: 'Bul\'kkk? Bul\'k! Bul\'k!',
+  noWay: 'Bul\'k!'
+};
+
+const playerActor = {
+  greeting: 'Oh, there you are! I was wondering where you were! I haven\'t see you since...',
+  exit: 'Yeah, I understand. Pretty traumatizing experience being dragged through the whole village by a cat, like some kind of flying mouse. I will leave you be then, get better soon!',
+  kittyJumpy: 'Riiiight..since the incident with Whiskers... Look, I\'m sorry! I didn\'t know that after drinking that potion he would be able to jump 5 meters!',
+  kittyNormal: 'Look, he\'s back to normal now, he will never be able to catch you again - you can come out!',
+  kittyApology: 'Whiskers is fine now! And I can tell by the look in his eyes that he\'s sorry! Come out, we miss you!',
+  comeAlong: 'I promise! How about this then, you can come and stick with me for some time and if he tries to do anything unnaturally skillful we will chill him out, together!',
+  questGet: 'Still no? Well, I guess I\'ll have to come up with some way to make it up to you...',
+  hireSuccess: 'Come on buddy, it\'s time to leave the cave and greet the world again! You can stick with me for some time and see how it goes.'
+};
+
 export const eyeballFirstTimeDialog: DialogTree = [{
   id: 'greetings',
-  text: 'Bul\'k bul\'k!',
+  text: eyeballActor.helloFriend,
   replies: [{
-    text: 'Oh, there you are! I was wondering where you were! I haven\'t see you since...',
+    text: playerActor.greeting,
     successTriggers: 'greetings2',
   }],
 }, {
   id: 'greetings2',
-  text: 'Buuuul\'k...',
+  text: eyeballActor.theAudacity,
   replies: [{
-    text: 'Riiiight..since the incident with Whiskers... Look, I\'m sorry! I didn\'t know that after drinking that potion he would be able to jump 5 meters!',
+    text: playerActor.kittyJumpy,
     successTriggers: 'greetings3',
   }],
 }, {
   id: 'greetings3',
-  text: 'Bul\'k! Bul\'k! Bul\'k!',
+  text: eyeballActor.howCouldYou,
   replies: [{
-    text: 'Look, he\'s back to normal now, he will never be able to catch you again - you can come out!',
+    text: playerActor.kittyNormal,
     successTriggers: 'greetings4',
   }],
 }, {
   id: 'greetings4',
-  text: 'Bul\'kkk? Bul\'k! Bul\'k!',
+  text: eyeballActor.unbelievable,
   replies: [{
-    text: 'I promise! How about this then, you can come and stick with me for some time and if he tries to do anything unnaturally skillful we will chill him out, together!',
+    text: playerActor.comeAlong,
     successTriggers: 'greetings5',
   }, {
-    text: 'Yeah, I understand. Pretty traumatizing experience being dragged through the whole village by a cat, like some kind of flying mouse. I will leave you be then, get better soon!',
+    text: playerActor.exit,
     callbackParam: 'fastEnd',
   }],
 }, {
   id: 'greetings5',
-  text: 'Bul\'k!',
+  text: eyeballActor.noWay,
   replies: [{
-    text: 'Still no? Well, I guess I\'ll have to come up with some way to make it up to you...',
+    text: playerActor.questGet,
     callbackParam: 'wantsToHelp',
   }, {
-    text: 'Yeah, I understand. Pretty traumatizing experience being dragged through the whole village by a cat, like some kind of flying mouse. I will leave you be then, get better soon!',
+    text: playerActor.exit,
     callbackParam: 'fastEnd',
   }],
 }];
 
 export const eyeballSecondTimeDialog: DialogTree = [{
   id: 'greetings4',
-  text: 'Bul\'kkk? Bul\'k! Bul\'k!',
+  text: eyeballActor.unbelievable,
   replies: [{
-    text: 'Whiskers is fine now! And I can tell by the look in his eyes that he\'s sorry! Come out, we miss you!',
+    text: playerActor.kittyApology,
     successTriggers: 'greetings5',
   }],
 }, {
   id: 'greetings5',
-  text: 'Bul\'k!',
+  text: eyeballActor.noWay,
   replies: [{
-    text: 'Still no? Well, I guess I\'ll have to come up with some way to make it up to you...',
+    text: playerActor.questGet,
     callbackParam: 'wantsToHelp',
   }, {
-    text: 'Yeah, I understand. Pretty traumatizing experience being dragged through the whole village by a cat, like some kind of flying mouse. I will leave you be then, get better soon!',
+    text: playerActor.exit,
     callbackParam: 'fastEnd',
   }],
 }];
 
 export const eyeballSecondTimeOfferPurplecupDialog: DialogTree = [{
   id: 'greetings4',
-  text: 'Bul\'kkk? Bul\'k! Bul\'k!',
+  text: eyeballActor.unbelievable,
   replies: [{
-    text: 'Whiskers is fine now! And I can tell by the look in his eyes that he\'s sorry! Come out, we miss you!',
+    text: playerActor.kittyApology,
     successTriggers: 'greetings5',
   }],
 }, {
   id: 'greetings5',
-  text: 'Bul\'k!',
+  text: eyeballActor.noWay,
   replies: [{
-    text: 'Come on buddy, it\'s time to leave the cave and greet the world again! You can stick with me for some time and see how it goes.',
+    text: playerActor.hireSuccess,
     callbackParam: 'eyeballJoined',
   }, {
-    text: 'Yeah, I understand. Pretty traumatizing experience being dragged through the whole village by a cat, like some kind of flying mouse. I will leave you be then, get better soon!',
+    text: playerActor.exit,
     callbackParam: 'fastEnd',
   }],
 }];
--- src/data/dialogs/betweenVillageAndDungeon/patchDialog.ts
+++ src/data/dialogs/betweenVillageAndDungeon/patchDialog.ts
@@ -1,31 +1,41 @@
 import { DialogTree } from '../../../types/my-types';
 
+const signActor = {
+  intro: `You are standing in front of your little patch. It's empty now because you just collected the crop of Sourgrass for your recent experiments. Usually it flourishes with all kinds of plants.
+  Not many in the village understand why you would grow so many inedible plants but you know that cabbage just doesn't cut it for really cool things. Aunt Nahkha is the only person who kind of gets it. She likes and grows all kinds of flowers.
+  So, would now be the time to start growing something new?`,
+  plantOptions: 'Based on the seeds and saplings you have with you, you could plant:'
+};
+
+const playerActor = {
+  intro: 'Let\'s see...',
+  exit: 'Nothing, today I simply don\'t have time for that.'
+};
+
 export const firstTimePatchDialog: DialogTree = [{
   id: 'patch1',
-  text: `You are standing in front of your little patch. It is empty now, because you just collected the crop of Sourgrass for your recent experiments, but usually it flourishes with all kinds of plants.
-    Not many in the village understand why would you grow so many uneatable plants, but you know that for really cool things cabbage just does not cut. Aunt Nahkha is the only person who kinda gets it - she likes and grows all kinds of flowers.
-    So, would now be the time to start growing something new?`,
+  text: signActor.intro,
   replies: [{
-    text: 'Let\'s see...',
+    text: playerActor.intro,
     successTriggers: 'selectPlants',
   }, {
-    text: 'No, today you simply don\'t have time for that',
+    text: playerActor.exit,
     callbackParam: 'fastEnd',
   }],
 }, {
   id: 'selectPlants',
-  text: 'Based on what seeds and saplings you have with you, you could plant:',
+  text: signActor.plantOptions,
   replies: [{
-    text: 'No, today you simply don\'t have time for that',
+    text: playerActor.exit,
     callbackParam: 'fastEnd',
   }],
 }];
 
 export const secondTimePatchDialog: DialogTree = [{
   id: 'selectPlants',
-  text: 'Based on what seeds and saplings you have with you, you could plant:',
+  text: signActor.plantOptions,
   replies: [{
-    text: 'No, today you simply don\'t have time for that',
+    text: playerActor.exit,
     callbackParam: 'fastEnd',
   }],
 }];
