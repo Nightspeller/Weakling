8794d8b688ade7f4993c33e8c8d6bade1aa6f44f
unknown <ctcrawford93@yahoo.com>
2021-06-21 18:09:57 -0400

    Proofreading for gravestone and test for dialog modification

--- src/data/dialogs/betweenVillageAndCaltor/graveDialog.ts
+++ src/data/dialogs/betweenVillageAndCaltor/graveDialog.ts
@@ -1,91 +1,85 @@
 import { DialogTree } from '../../../types/my-types';
 
+const graveActor = {
+  intro: `(While walking through the woods you find yourself in a little glade, brightly lit by the sun. The flowers under your feet are bright and soft; the air is fresh and clean, and the surroundings are filled with peace and sorrow. 
+    In the middle of the glade you see a lonely grave, old, but mostly untouched by time. In front of it there is a sword stuck into the dirt. The light reflects from its edge, but also passes through, leaving very little shade.)`,
+  engraving: `    The engraving says:
+    
+  Here lies the great hero of Caltor,
+  Defender of the weak and protector of the destitute.
+  Call his name, glorify his deeds, mourn his fall, swear his oath
+  And be blessed with his power to continue his course.`
+};
+
+const playerActor = {
+  intro: 'Let\'s see...',
+  exit: '(Leave)',
+  badChoice: 'The name of this hero is Jeremy von Caltor',
+  goodChoice: 'The name of this hero is Jeremaya the Bandit',
+  deedsGlorified: 'The deeds of sir Jeremaya are glorified on the central square of Caltor.',
+  oathTaken: 'Sir Jeremaya, your good name was called and glorified, your death is moaned. Now, let me take the oath and continue your course'
+};
+
 export const graveDialog: DialogTree = [{
   id: 'grave1',
-  text: `(Walking through the woods, you find yourself on the little glade, brightly lit by the sun. Flowers under your feet are bright and soft, the air is fresh and clean and the surroundings are filled with piece and sorrow. 
-    In the middle of the glade, you see a lonely grave, old, but mostly untouched by the time. In front of it there is a sword, stuck into the dirt. The light reflects from it's edge, but also passing through, leaving very little shade.)`,
+  text: graveActor.intro,
   replies: [{
-    text: 'Let\'s see...',
+    text: playerActor.intro,
     successTriggers: 'grave2',
   }],
 }, {
   id: 'grave2',
-  text: `    The engraving says:
-    
-    Here lays the great hero of Caltor, 
-    Defender of the weak and protector of destitute.
-    Call his name, glorify his deeds, moan his fall, swear his oath 
-    And be blessed with his power to continue his course.`,
+  text: graveActor.engraving,
   replies: [{
-    text: 'Leave',
+    text: playerActor.exit,
     callbackParam: 'fastEnd',
   }],
 }];
 
 export const graveDialogWithFalseName: DialogTree = [{
   id: 'grave1',
-  text: `    The engraving says:
-    
-    Here lays the great hero of Caltor, 
-    Defender of the weak and protector of destitute.
-    Call his name, glorify his deeds, moan his fall, swear his oath 
-    And be blessed with his power to continue his course.`,
+  text: graveActor.engraving,
   replies: [{
-    text: 'The name of this hero is Jeremy von Caltor',
+    text: playerActor.badChoice,
     callbackParam: 'falseNameCalled',
   }, {
-    text: '(Leave)',
+    text: playerActor.exit,
     callbackParam: 'fastEnd',
   }],
 }];
 
 export const graveDialogWithTrueName: DialogTree = [{
   id: 'grave1',
-  text: `    The engraving says:
-    
-    Here lays the great hero of Caltor, 
-    Defender of the weak and protector of destitute.
-    Call his name, glorify his deeds, moan his fall, swear his oath 
-    And be blessed with his power to continue his course.`,
+  text: graveActor.engraving,
   replies: [{
-    text: 'The name of this hero is Jeremaya the Bandit',
+    text: playerActor.goodChoice,
     callbackParam: 'trueNameCalled',
   }, {
-    text: '(Leave)',
+    text: playerActor.exit,
     callbackParam: 'fastEnd',
   }],
 }];
 
 export const graveDialogWithGlorification: DialogTree = [{
   id: 'grave1',
-  text: `    The engraving says:
-    
-    Here lays the great hero of Caltor, 
-    Defender of the weak and protector of destitute.
-    Call his name, glorify his deeds, moan his fall, swear his oath 
-    And be blessed with his power to continue his course.`,
+  text: graveActor.engraving,
   replies: [{
-    text: 'The deeds of sir Jeremaya are glorified on the central square of Caltor.',
+    text: playerActor.deedsGlorified,
     callbackParam: 'deedsGlorified',
   }, {
-    text: '(Leave)',
+    text: playerActor.exit,
     callbackParam: 'fastEnd',
   }],
 }];
 
 export const readyToTakeTheOath: DialogTree = [{
   id: 'grave1',
-  text: `    The engraving says:
-    
-    Here lays the great hero of Caltor, 
-    Defender of the weak and protector of destitute.
-    Call his name, glorify his deeds, moan his fall, swear his oath 
-    And be blessed with his power to continue his course.`,
+  text: graveActor.engraving,
   replies: [{
-    text: 'Sir Jeremaya, your good name was called and glorified, your death is moaned. Now, let me take the oath and continue your course',
+    text: playerActor.oathTaken,
     callbackParam: 'oathTaken',
   }, {
-    text: '(Leave)',
+    text: playerActor.exit,
     callbackParam: 'fastEnd',
   }],
 }];
