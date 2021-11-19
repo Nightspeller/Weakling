4e5ae4cc0851c3df072d1d792831f3dcc3e61b6f
Johannes <johannes-magnusson@hotmail.com>
2021-11-19 01:03:56 +0100

    Added new intro scene

--- src/scenes/intro-and-main-menu/intro.ts
+++ src/scenes/intro-and-main-menu/intro.ts
@@ -1,8 +1,10 @@
-import * as Phaser from 'phaser';
-import { GAME_H, GAME_W } from '../../config/constants';
+import GeneralLocation from '../locations/generalLocation';
 import BackgroundSoundScene from '../backgroundSoundScene';
 
-export default class IntroScene extends Phaser.Scene {
+import {introRevivalDialog} from '../../data/dialogs/introRevivalDialog';
+import { sceneEvents } from '../../triggers/eventsCenter';
+
+export default class IntroScene extends GeneralLocation {
   constructor() {
     super({ key: 'Intro' });
   }
@@ -10,55 +12,26 @@ export default class IntroScene extends Phaser.Scene {
   preload() {
   }
 
+  public init(data: any) {
+    super.init(data);
+  }
   create() {
     this.cameras.main.fadeIn(2000, 0, 0, 0);
 
     const soundManagerScene = this.scene.get('BackgroundSound') as BackgroundSoundScene;
     soundManagerScene.playBackgroundMusic('intro');
 
-    /*    const bgVoice = this.sound.add('intro-voice', {
-      volume: 0, // currently disabled since audio probably should be re-recorded
-    });
+    this.scene.run('StoryTellingWindow');
 
-    bgVoice.play(); */
-
-    this.add.text(GAME_W / 2, GAME_H / 2 - 70,
-      'Still... Stay still... Calm... Stay calm... \n\n'
-      + 'You do not have to go, you do not have to do anything at all...\n\n'
-      + 'Just stay that incredibly still..and listen to my voice.\n\n\n'
-      + 'I am here to help. I Can help you, and I Will. But I need you to help me first.\n\n'
-      + 'Concentrate.. Concentrate on my voice, stay still...and Think. \n\n'
-      + 'I need you to help me, I need you to give me a Reason.\n\n'
-      + 'A reason to help you.\n\n'
-      + 'So ... Think. Just Think...\n\n\n'
-      + 'Good... Very good.\n\n\n'
-      + 'Now...take me there, show me...',
-      {
-        font: '14px monospace',
-        color: '#ffffff',
-      })
-      .setOrigin(0.5, 0.5);
-
-    const showHimVillage = this.add.text(GAME_W / 2, GAME_H / 2 + 200,
-      'Show him that lovely morning at the Village',
-      {
-        font: '14px monospace',
-        color: '#ffffff',
-        backgroundColor: '#222222',
-        padding: {
-          x: 10,
-          y: 10,
+    this.time.delayedCall(7500, () => {
+    this.switchToScene('Dialog', {
+      dialogTree: introRevivalDialog,
+      speakerName: 'Unknown'
         },
-      })
-      .setOrigin(0.5, 0.5)
-      .setInteractive({ useHandCursor: true });
-
-    this.add.graphics()
-      .lineStyle(2, 0xffffff, 0.4)
-      .strokeRect(showHimVillage.getTopLeft().x, showHimVillage.getTopLeft().y, showHimVillage.width, showHimVillage.height);
-
-    showHimVillage.once('pointerdown', () => {
-      // bgVoice.stop();
+      closeCallback: () => {
+        this.cameras.main.fadeOut(1500, 0, 0, 0);
+        this.scene.remove('StoryTellingWindow');
+          this.time.delayedCall(1500, () => {
       this.scene.start('WeaklingsCave', { prevScene: this.scene.key });
     });
 
@@ -72,6 +45,11 @@ export default class IntroScene extends Phaser.Scene {
           x: 10,
           y: 10,
         },
+      updateCallback: () => {
+        console.log('updateCallback')
+        sceneEvents.emit('change-intro-story-video')
+      }
+    }, false);
       })
       .setOrigin(0.5, 0.5)
       .setInteractive({ useHandCursor: true });
